b948013a79830401c266d76a132b5ad3ea73ed07
Alejandra Cáceres <alejandraicaceres@gmail.com>
2023-04-08 11:48:10 -0300

    Make querybuilder for product promotions

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\DTO\LowestPriceEnquiry;
+use App\Entity\Promotion;
 use App\Filter\PromotionsFilterInterface;
+use App\Repository\ProductRepository;
 use App\Service\Serializer\DTOSerializer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +16,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
+
+    public function __construct(
+        private ProductRepository $repository,
+        private EntityManagerInterface $entityManager,
+        )
+    {
+    }
+
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
     public function lowestPrice(
         Request $request,
@@ -30,6 +41,14 @@ class ProductController extends AbstractController
 
         $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
 
+        $product = $this->repository->find($id);
+        // add error handling for not found
+        $lowestPriceEnquiry->setProduct($product);
+        $promotions = $this->entityManager->getRepository(Promotion::class)->findValidForProduct(
+            $product,
+            date_create_immutable($lowestPriceEnquiry->getRequestDate())
+        );
+dd($promotions);
         $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry);
 
         $responseContent = $serializer->serialize($modifiedEnquiry, 'json');
